cf0d528a80e40efce7ac8889a89769580b3ee8a6
Jinzhen Wang <jw447@njit.edu>
2020-01-03 17:41:33 -0500

    .

--- src/cuda_zfp/encode.cuh
+++ src/cuda_zfp/encode.cuh
@@ -332,6 +332,7 @@ void inline __device__ zfp_encode_block(Scalar *fblock,
                                         const uint block_idx,
                                         Word *stream)
 {
+  //jwang
   BlockWriter<BlockSize> block_writer(stream, maxbits, block_idx);
   int emax = max_exponent<Scalar, BlockSize>(fblock);
   int maxprec = precision(emax, get_precision<Scalar>(), get_min_exp<Scalar>());
